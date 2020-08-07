49a99858dd4237db42fb5bbc1faa3b772303f957
Chelsea Saunders <c_chilcoat@msn.com>
2020-08-07 19:38:37 +0000

    Adding completed exercises from Methods chapter

---
+++ Methods/greeting.rb
@@ -0,0 +1,16 @@
+def greeting(name)
+  "Oh hi " + name + "!"
+end
+
+puts greeting("Mark")
+
+
+1. x = 2 # => 2
+
+2. puts x = 2 # => nil
+
+3. p name = "Joe" # => "Joe"
+
+4. four = "four" # => "four"
+
+5. print something = "nothing" # => nil
